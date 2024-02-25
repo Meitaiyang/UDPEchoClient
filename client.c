3bc24e144f0a7777afb9039c611d288194bdb23a
Meitaiyang <shrek473@gmail.com>
2024-02-25 23:19:29 +0800

    'fork' for client

--- client.c
+++ client.c
@@ -1,39 +1,73 @@
-// server program for udp connection 
-#include <stdio.h> 
-#include <strings.h> 
-#include <sys/types.h> 
-#include <arpa/inet.h> 
-#include <sys/socket.h> 
-#include<netinet/in.h> 
-#define PORT 5000 
-#define MAXLINE 1000 
-  
-// Driver code 
-int main() 
-{    
-    char buffer[100]; 
-    char *message = "Hello Client"; 
-    int listenfd, len; 
-    struct sockaddr_in servaddr, cliaddr; 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // Create a UDP Socket 
-    listenfd = socket(AF_INET, SOCK_DGRAM, 0);         
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_family = AF_INET;  
-   
-    // bind server address to socket descriptor 
-    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
-       
-    //receive the datagram 
-    len = sizeof(cliaddr); 
-    int n = recvfrom(listenfd, buffer, sizeof(buffer), 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
-    buffer[n] = '\0'; 
-    puts(buffer); 
-           
-    // send the response 
-    sendto(listenfd, message, MAXLINE, 0, 
-          (struct sockaddr*)&cliaddr, sizeof(cliaddr)); 
-} 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h> // For string functions
+
+#define BUFFER_SIZE 256 // Maximum buffer size for the string
+#define MAX_WAIT_INTERVAL 8000
+#define BASE 500
+
+int main()
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    int multiplier = 2;
+    int retry_time = 0;
+    double wait_interval=0;
+
+    char buffer[BUFFER_SIZE]; // Buffer for the string
+
+    // Example string to send
+    const char *message = "Hello, server!";
+
+    // Create a socket for the client.
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Name the socket, as agreed with the server.
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = 9734;
+    len = sizeof(address);
+
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    printf("%d\n", result);
+
+    while (result == -1 && wait_interval < MAX_WAIT_INTERVAL){
+        wait_interval = BASE * pow((double)multiplier, (double)(retry_time++));
+        printf("%f\n", wait_interval);
+        usleep((int)wait_interval*1000);
+    
+        result = connect(sockfd, (struct sockaddr *)&address, len);
+    } 
+
+    if (result == -1)
+    {
+        perror("oops: client");
+        exit(0);
+    }
+
+    // We can now read/write via sockfd.
+
+    // Send the message to the server
+    write(sockfd, message, strlen(message));
+
+    // Read the response from the server
+    ssize_t bytes_read = read(sockfd, buffer, BUFFER_SIZE);
+    if (bytes_read > 0)
+    {
+        // Ensure the received data is null-terminated
+        buffer[bytes_read] = '\0';
+        printf("Received from server: %s\n", buffer);
+    }
+
+    close(sockfd);
+    return 1;
+}
+
--- server.c
+++ server.c
@@ -1,49 +1,63 @@
-// udp client driver program 
-#include <stdio.h> 
-#include <strings.h> 
-#include <sys/types.h> 
-#include <arpa/inet.h> 
-#include <sys/socket.h> 
-#include<netinet/in.h> 
-#include<unistd.h> 
-#include<stdlib.h> 
-  
-#define PORT 5000 
-#define MAXLINE 1000 
-  
-// Driver code 
-int main() 
-{    
-    char buffer[100]; 
-    char *message = "Hello Server"; 
-    int sockfd, n; 
-    struct sockaddr_in servaddr; 
-      
-    // clear servaddr 
-    bzero(&servaddr, sizeof(servaddr)); 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_family = AF_INET; 
-      
-    // create datagram socket 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-      
-    // connect to server 
-    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
-    { 
-        printf("\n Error : Connect Failed \n"); 
-        exit(0); 
-    } 
-  
-    // request to send datagram 
-    // no need to specify server address in sendto 
-    // connect stores the peers IP and port 
-    sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)NULL, sizeof(servaddr)); 
-      
-    // waiting for response 
-    recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL); 
-    puts(buffer); 
-  
-    // close the descriptor 
-    close(sockfd); 
-} 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define BUFFER_SIZE 256
+
+int main()
+{
+    int server_sockfd, client_sockfd;
+    int server_len, client_len;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+
+    /* Create an unnamed socket for the server. */
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* Name the socket. */
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_port = 9734;
+    server_len = sizeof(server_address);
+    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+
+    /* Create a connection queue and wait for clients. */
+    listen(server_sockfd, 5); 
+
+    signal(SIGCHLD, SIG_IGN);
+
+    while(1) {
+        char buffer[BUFFER_SIZE]; 
+
+        printf("server waiting\n");
+
+        /* Accept a connection. */
+        client_len = sizeof(client_address);
+        client_sockfd = accept(server_sockfd, 
+            (struct sockaddr *)&client_address, &client_len);
+
+        /* We can now read/write to client on client_sockfd. */
+        ssize_t bytes_read = read(client_sockfd, buffer, BUFFER_SIZE);
+        if(fork() == 0){
+            if (bytes_read > 0) {
+                printf("Received: %s\n", buffer);
+                
+                // Echo back the modified string to the client
+                write(client_sockfd, buffer, bytes_read); 
+                close(client_sockfd);
+            }
+
+        } else {
+            close(client_sockfd);
+            exit(0);
+        }
+
+    }   
+    return 0;
+}
+
